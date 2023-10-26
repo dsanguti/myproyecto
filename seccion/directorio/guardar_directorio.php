841a2e54ffcb63e2bcaf0db32596d79359b24e3a
dsanguti <dsanguti@gmail.com>
2023-10-26 13:29:50 +0200

    Insertar Contacto Directorio Funciona

--- seccion/directorio/guardar_directorio.php
+++ seccion/directorio/guardar_directorio.php
@@ -1,2 +1,23 @@
 <?php
-include_once('./bd/directorio/conector_BD_directorio.php');
+include_once('../../bd/directorio/conector_BD_directorio.php');
+if (isset($_POST['guardar_directorio'])) {
+    $database = new ConectarBD();
+    $db = $database->abrir();
+    try {
+        $stmt = $db->prepare("INSERT INTO directorio (puesto,  nombre, apellidos, oficina, telefono, extension, correo)
+        VALUES (:puestoDirectorio, :nombre_directorio, :apellidos_directorio, :oficina_directorio, :telefono_directorio, :extension_directorio, :correo_directorio)");
+
+        $_SESSION['message'] = ($stmt->execute(array(
+            ':puestoDirectorio' => $_POST['puestoDirectorio'],
+            ':nombre_directorio' => $_POST['nombre_directorio'], ':apellidos_directorio' => $_POST['apellidos_directorio'], ':oficina_directorio' => $_POST['oficina_directorio'],
+            ':telefono_directorio' => $_POST['telefono_directorio'], ':extension_directorio' => $_POST['extension_directorio'], ':correo_directorio' => $_POST['correo_directorio']
+        )))
+            ? 'Contacto agregado' : 'error al insertar';
+    } catch (PDOException $e) {
+        $_SESSION['message'] = $e->getMessage();
+    }
+    $database->cerrar();
+} else {
+    $_SESSION['message'] = 'Rellene el formulario';
+}
+header('location: http://localhost/myproyecto/#/directorio');
