7303b448a40a7f38f9a9e90bbd054fe6329a7166
dsanguti <dsanguti@gmail.com>
2023-12-21 13:31:23 +0100

    directorio.  fija cabecezar scroll vertical

--- seccion/directorio/directorio.php
+++ seccion/directorio/directorio.php
@@ -13,10 +13,10 @@ session_start();
                 <?php
                 if ($_SESSION["directorio"] === "editar") {
                 ?>
-                <div class="col mt-4">
-                    <a href="#insertarModal" class="btn btn-primary" data-bs-toggle="modal"><i class="bi bi-plus"></i>
-                        Nuevo</a>
-                </div>
+                    <div class="col mt-4">
+                        <a href="#insertarModal" class="btn btn-primary" data-bs-toggle="modal"><i class="bi bi-plus"></i>
+                            Nuevo</a>
+                    </div>
                 <?php
                 }
                 ?>
@@ -24,8 +24,7 @@ session_start();
 
                 <div class="col mt-4 d-flex justify-content-end">
                     <form role="search">
-                        <input style="max-width:450px;" class="form-control me-2" type="search"
-                            placeholder="Buscar en directorio" id="buscarDirectorio" aria-label="Search">
+                        <input style="max-width:450px;" class="form-control me-2" type="search" placeholder="Buscar en directorio" id="buscarDirectorio" aria-label="Search">
 
                     </form>
                 </div>
@@ -59,103 +58,85 @@ session_start();
                         $result = $conn->query($query);
                         ?>
 
-                        <table class="table table-striped table-hover">
-                            <!-- Las cabeceras de la tabla directorio-->
-                            <thead>
-                                <tr>
-                                    <th style="display:none;" scope="col">id</th>
-                                    <th style="text-align:center;" scope="col">Puesto</th>
-                                    <th style="text-align:center;" scope="col">Nombre</th>
-                                    <th style="text-align:center;" scope="col">Apellidos</th>
-                                    <th style="text-align:center;" scope="col">Oficina</th>
-                                    <th style="text-align:center;" scope="col">Teléfono</th>
-                                    <th style="text-align:center;" scope="col">Extensión</th>
-                                    <th style="text-align:center;" scope="col">Correo</th>
-
-                                    <?php
-                                    if ($_SESSION["directorio"] === "editar") {
-                                    ?>
-                                    <th style="text-align:center;color:orange;" scope="col">Edit</th>
-                                    <th style="text-align:center;color:red;" scope="col">Del</th>
-                                    <?php
-                                    }
-                                    ?>
-
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <?php while ($row = $result->fetch_assoc()) : ?>
-                                <tr class="celda_tabla_directorio">
-
-                                    <td style='display:none;'><?php echo $row['id'] ?></td>
-                                    <td style='text-align:center;'><?php echo $row['puesto'] ?>
-                                    </td>
-                                    <td style='text-align:center;'><?php echo $row['nombre'] ?>
-                                    </td>
-                                    <td style='text-align:center;'>
-                                        <?php echo $row['apellidos'] ?></td>
-                                    <td style='text-align:center;'><?php echo $row['oficina'] ?>
-                                    </td>
-                                    <td style='text-align:center;'><?php echo $row['telefono'] ?>
-                                    </td>
-                                    <td style='text-align:center;'>
-                                        <?php echo $row['extension'] ?></td>
-                                    <td style='text-align:center;'><?php echo $row['correo'] ?>
-                                    </td>
-                                    <?php
-
-                                        if ($_SESSION["directorio"] === "editar") {
-                                        ?>
-                                    <td style='text-align:center;'> <button id='btn-edit-directorio' class='edit-table'
-                                            data-bs-toggle='modal'
-                                            data-bs-target='#modal-edit-directorio<?php echo $row['id']; ?>'>
-                                            <i class='bi bi-pencil-square'></i></button>
-                                    <td style='text-align:center;'> <button id='btn-del-directorio' class='del-table'
-                                            data-bs-toggle='modal'
-                                            data-bs-target='#modal-del-directorio<?php echo $row['id']; ?>'><i
-                                                class='bi bi-trash'></i></button>
-                                        <!-- El modal de editar-->
-                                        <?php include("./modal/modal_editar.php"); ?>
-
-                                        <!-- El modal de Eliminar-->
-                                        <?php include("./modal/modal_eliminar.php");
-                                                ?>
+                        <div class="table-responsive table-fixed">
+                            <table class="table table-striped table-hover" id="table-directorio">
+                                <!-- Las cabeceras de la tabla directorio-->
+                                <thead style="position: sticky;top:0;">
+                                    <tr>
+                                        <th style="display:none;" scope="col">id</th>
+                                        <th style="background-color:#0f6ba5;color: #fff;text-align:center;" scope="col">
+                                            Puesto</th>
+                                        <th style="background-color:#0f6ba5;color: #fff;text-align:center;" scope="col">
+                                            Nombre</th>
+                                        <th style="background-color:#0f6ba5;color: #fff;text-align:center;" scope="col">
+                                            Apellidos</th>
+                                        <th style="background-color:#0f6ba5;color: #fff;text-align:center;" scope="col">
+                                            Oficina</th>
+                                        <th style="background-color:#0f6ba5;color: #fff;text-align:center;" scope="col">
+                                            Teléfono</th>
+                                        <th style="background-color:#0f6ba5;color: #fff;text-align:center;" scope="col">
+                                            Extensión</th>
+                                        <th style="background-color:#0f6ba5;color: #fff;text-align:center;" scope="col">
+                                            Correo</th>
 
                                         <?php
+                                        if ($_SESSION["directorio"] === "editar") {
+                                        ?>
+                                            <th style="background-color:#0f6ba5;color: #fff;text-align:center;color:orange;" scope="col">Edit</th>
+                                            <th style="background-color:#0f6ba5;color: #fff;text-align:center;color:red;" scope="col">Del</th>
+                                        <?php
                                         }
+                                        ?>
+
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php while ($row = $result->fetch_assoc()) : ?>
+                                        <tr class="celda_tabla_directorio">
+
+                                            <td style='display:none;'><?php echo $row['id'] ?></td>
+                                            <td style='text-align:center;'><?php echo $row['puesto'] ?>
+                                            </td>
+                                            <td style='text-align:center;'><?php echo $row['nombre'] ?>
+                                            </td>
+                                            <td style='text-align:center;'>
+                                                <?php echo $row['apellidos'] ?></td>
+                                            <td style='text-align:center;'><?php echo $row['oficina'] ?>
+                                            </td>
+                                            <td style='text-align:center;'><?php echo $row['telefono'] ?>
+                                            </td>
+                                            <td style='text-align:center;'>
+                                                <?php echo $row['extension'] ?></td>
+                                            <td style='text-align:center;'><?php echo $row['correo'] ?>
+                                            </td>
+                                            <?php
+
+                                            if ($_SESSION["directorio"] === "editar") {
                                             ?>
+                                                <td style='text-align:center;'> <button id='btn-edit-directorio' class='edit-table' data-bs-toggle='modal' data-bs-target='#modal-edit-directorio<?php echo $row['id']; ?>'>
+                                                        <i class='bi bi-pencil-square'></i></button>
+                                                <td style='text-align:center;'> <button id='btn-del-directorio' class='del-table' data-bs-toggle='modal' data-bs-target='#modal-del-directorio<?php echo $row['id']; ?>'><i class='bi bi-trash'></i></button>
+                                                    <!-- El modal de editar-->
+                                                    <?php include("./modal/modal_editar.php"); ?>
 
-                                </tr>
+                                                    <!-- El modal de Eliminar-->
+                                                    <?php include("./modal/modal_eliminar.php");
+                                                    ?>
 
-                                <?php endwhile; ?>
+                                                <?php
+                                            }
+                                                ?>
 
-                            </tbody>
-                        </table>
+                                        </tr>
+
+                                    <?php endwhile; ?>
+
+                                </tbody>
+                            </table>
+                        </div>
+
                     </div>
 
-
-
-                    <!--PAGINACIÓN DE LA TABLA-->
-                    <nav aria-label="Page navigation example">
-                        <ul class="pagination" id="pagination-directorio">
-                            <li class="page-item <?php echo ($pagina <= 1) ? 'disabled' : ''; ?>">
-                                <a class="page-link" data-page="<?php echo ($pagina - 1) ?>"
-                                    href="#/directorio?pagina=<?php echo ($pagina - 1); ?>">Anterior</a>
-                            </li>
-
-                            <?php for ($i = 1; $i <= $total_paginas; $i++) : ?>
-                            <li class="page-item <?php echo ($pagina == $i) ? 'active' : ''; ?>">
-                                <a class="page-link" data-page="<?php echo $i ?>"
-                                    href="#/directorio?pagina=<?php echo $i; ?>"><?php echo $i; ?></a>
-                            </li>
-                            <?php endfor; ?>
-
-                            <li class="page-item <?php echo ($pagina >= $total_paginas) ? 'disabled' : ''; ?>">
-                                <a class="page-link" data-page="<?php echo ($pagina + 1) ?>"
-                                    href="#/directorio?pagina=<?php echo ($pagina + 1); ?>">Siguiente</a>
-                            </li>
-                        </ul>
-                    </nav>
                 </div>
                 <!-- El modal de editar-->
                 <?php
