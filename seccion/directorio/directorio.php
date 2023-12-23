94cc7e8facae9974e7bef0d2800230f10f8e0793
dsanguti <dsanguti@gmail.com>
2023-12-23 22:39:50 +0100

    correccion boton nuevo directorio

--- seccion/directorio/directorio.php
+++ seccion/directorio/directorio.php
@@ -13,10 +13,10 @@ session_start();
                 <?php
                 if ($_SESSION["directorio"] === "editar") {
                 ?>
-                <div class="col mt-4">
-                    <a href="#insertarModal" class="btn btn-primary" data-bs-toggle="modal"><i class="bi bi-plus"></i>
-                        Nuevo</a>
-                </div>
+                    <div class="col mt-4">
+                        <a href="#insertarModal" class="btn btn-primary" data-bs-toggle="modal"><i class="bi bi-plus"></i>
+                            Nuevo</a>
+                    </div>
                 <?php
                 }
                 ?>
@@ -25,10 +25,8 @@ session_start();
                 <div class="col mt-4 d-flex justify-content-end">
                     <form class="d-flex flex-row" role="search">
                         <div class="input-container-search">
-                            <input style="max-width:450px;" class="form-control me-2 input-dir-search" type="search"
-                                placeholder="Buscar en directorio" id="buscarDirectorio" aria-label="Search">
-                            <i id="xSearchDir" class="bi bi-x-circle clear-search-dir icon-clear-dir"
-                                style="padding-right: 7px;" onclick="CerrarIconDirBuscar()"></i>
+                            <input style="max-width:450px;" class="form-control me-2 input-dir-search" type="search" placeholder="Buscar en directorio" id="buscarDirectorio" aria-label="Search">
+                            <i id="xSearchDir" class="bi bi-x-circle clear-search-dir icon-clear-dir" style="padding-right: 7px;" onclick="CerrarIconDirBuscar()"></i>
                         </div>
 
                     </form>
@@ -49,18 +47,10 @@ session_start();
 
                         <?php include_once('../../bd/directorio/conector_BD_directorio.php');
 
-                        //Obtener el número total de registros y calcular el número total de páginas,cálculos para la paginación de la tabla directorio
-                        $result = $conn->query("SELECT COUNT(*) as total FROM directorio");
+                        //Obtener el número total de registros 
+                        $result = $conn->query("SELECT * FROM directorio");
                         $row = $result->fetch_assoc();
-                        $total_registros = $row['total'];
-                        $registros_por_pagina = 4;
-                        $total_paginas = ceil($total_registros / $registros_por_pagina);
 
-                        $pagina = isset($_GET['pagina']) ? $_GET['pagina'] : 1;
-                        $offset = ($pagina - 1) * $registros_por_pagina;
-
-                        $query = "SELECT * FROM directorio LIMIT $offset, $registros_por_pagina";
-                        $result = $conn->query($query);
                         ?>
 
                         <div class="table-responsive table-fixed">
@@ -87,10 +77,8 @@ session_start();
                                         <?php
                                         if ($_SESSION["directorio"] === "editar") {
                                         ?>
-                                        <th style="background-color:#0f6ba5;color: #fff;text-align:center;color:orange;"
-                                            scope="col">Edit</th>
-                                        <th style="background-color:#0f6ba5;color: #fff;text-align:center;color:red;"
-                                            scope="col">Del</th>
+                                            <th style="background-color:#0f6ba5;color: #fff;text-align:center;color:orange;" scope="col">Edit</th>
+                                            <th style="background-color:#0f6ba5;color: #fff;text-align:center;color:red;" scope="col">Del</th>
                                         <?php
                                         }
                                         ?>
@@ -99,47 +87,42 @@ session_start();
                                 </thead>
                                 <tbody>
                                     <?php while ($row = $result->fetch_assoc()) : ?>
-                                    <tr class="celda_tabla_directorio">
+                                        <tr class="celda_tabla_directorio">
 
-                                        <td style='display:none;'><?php echo $row['id'] ?></td>
-                                        <td style='text-align:center;'><?php echo $row['puesto'] ?>
-                                        </td>
-                                        <td style='text-align:center;'><?php echo $row['nombre'] ?>
-                                        </td>
-                                        <td style='text-align:center;'>
-                                            <?php echo $row['apellidos'] ?></td>
-                                        <td style='text-align:center;'><?php echo $row['oficina'] ?>
-                                        </td>
-                                        <td style='text-align:center;'><?php echo $row['telefono'] ?>
-                                        </td>
-                                        <td style='text-align:center;'>
-                                            <?php echo $row['extension'] ?></td>
-                                        <td style='text-align:center;'><?php echo $row['correo'] ?>
-                                        </td>
-                                        <?php
+                                            <td style='display:none;'><?php echo $row['id'] ?></td>
+                                            <td style='text-align:center;'><?php echo $row['puesto'] ?>
+                                            </td>
+                                            <td style='text-align:center;'><?php echo $row['nombre'] ?>
+                                            </td>
+                                            <td style='text-align:center;'>
+                                                <?php echo $row['apellidos'] ?></td>
+                                            <td style='text-align:center;'><?php echo $row['oficina'] ?>
+                                            </td>
+                                            <td style='text-align:center;'><?php echo $row['telefono'] ?>
+                                            </td>
+                                            <td style='text-align:center;'>
+                                                <?php echo $row['extension'] ?></td>
+                                            <td style='text-align:center;'><?php echo $row['correo'] ?>
+                                            </td>
+                                            <?php
 
                                             if ($_SESSION["directorio"] === "editar") {
                                             ?>
-                                        <td style='text-align:center;'> <button id='btn-edit-directorio'
-                                                class='edit-table' data-bs-toggle='modal'
-                                                data-bs-target='#modal-edit-directorio<?php echo $row['id']; ?>'>
-                                                <i class='bi bi-pencil-square'></i></button>
-                                        <td style='text-align:center;'> <button id='btn-del-directorio'
-                                                class='del-table' data-bs-toggle='modal'
-                                                data-bs-target='#modal-del-directorio<?php echo $row['id']; ?>'><i
-                                                    class='bi bi-trash'></i></button>
-                                            <!-- El modal de editar-->
-                                            <?php include("./modal/modal_editar.php"); ?>
+                                                <td style='text-align:center;'> <button id='btn-edit-directorio' class='edit-table' data-bs-toggle='modal' data-bs-target='#modal-edit-directorio<?php echo $row['id']; ?>'>
+                                                        <i class='bi bi-pencil-square'></i></button>
+                                                <td style='text-align:center;'> <button id='btn-del-directorio' class='del-table' data-bs-toggle='modal' data-bs-target='#modal-del-directorio<?php echo $row['id']; ?>'><i class='bi bi-trash'></i></button>
+                                                    <!-- El modal de editar-->
+                                                    <?php include("./modal/modal_editar.php"); ?>
 
-                                            <!-- El modal de Eliminar-->
-                                            <?php include("./modal/modal_eliminar.php");
+                                                    <!-- El modal de Eliminar-->
+                                                    <?php include("./modal/modal_eliminar.php");
                                                     ?>
 
-                                            <?php
+                                                <?php
                                             }
                                                 ?>
 
-                                    </tr>
+                                        </tr>
 
                                     <?php endwhile; ?>
 
