1ee0a57856e9cc21649b4a0ab06d1ccb9eba9e65
dsanguti <dsanguti@gmail.com>
2024-01-04 16:42:00 +0100

    Se pone color al reporte dir. y configura pie pagina

--- fpdf/directorio/genera_dir.php
+++ fpdf/directorio/genera_dir.php
@@ -6,14 +6,20 @@ class PDF extends FPDF
     // Cabecera de página
     function Header()
     {
+        // Configurar el margen izquierdo
+        $this->SetLeftMargin(30);
+
         // Logo
-        $this->Image('../../img/fondo_login.jpg', 10, 8, 33);
+        $this->Image('../../img/fondo_login.jpg', 15, 8, 33);
         // Arial bold 15
         $this->SetFont('Arial', 'B', 15);
+
+        // Color al título
+        $this->SetTextColor(15, 107, 165);
         // Movernos a la derecha
-        $this->Cell(80);
+        $this->Cell(100);
         // Título
-        $this->Cell(30, 10, 'Directorio SEPE Melillla', 0, 1, 'C');
+        $this->Cell(30, 10, 'Directorio SEPE Melilla', 0, 1, 'C');
         // Salto de línea
         $this->Ln(20);
     }
@@ -25,8 +31,21 @@ class PDF extends FPDF
         $this->SetY(-15);
         // Arial italic 8
         $this->SetFont('Arial', 'I', 8);
-        // Número de página
-        $this->Cell(0, 10, 'Page ' . $this->PageNo() . '/{nb}', 0, 0, 'C');
+
+        // Alias para el número total de páginas
+        $this->AliasNbPages();
+
+        // Número de página actual
+        $pageNumber = $this->PageNo();
+
+        // Texto del pie de página con número de página
+        $text = 'Página ' . $pageNumber . '/{nb}';
+
+        // Aplicar mb_convert_encoding si es necesario
+        $text = mb_convert_encoding($text, 'ISO-8859-1', 'UTF-8');
+
+        // Agregar el texto al pie de página
+        $this->Cell(0, 10, $text, 0, 0, 'C');
     }
 }
 
@@ -47,18 +66,26 @@ $pdf->AddPage();
 
 // Configurar la cabecera de la tabla
 $pdf->SetFont('Arial', 'B', 10);
-$pdf->Cell(58, 10, mb_convert_encoding('Puesto', 'ISO-8859-1', 'UTF-8'), 1, 0, 'C');
-$pdf->Cell(30, 10, mb_convert_encoding('Nombre', 'ISO-8859-1', 'UTF-8'), 1, 0, 'C');
-$pdf->Cell(40, 10, mb_convert_encoding('Apellidos', 'ISO-8859-1', 'UTF-8'), 1, 0, 'C');
-$pdf->Cell(28, 10, mb_convert_encoding('Oficina', 'ISO-8859-1', 'UTF-8'), 1, 0, 'C');
-$pdf->Cell(25, 10, mb_convert_encoding('Teléfono', 'ISO-8859-1', 'UTF-8'), 1, 0, 'C');
-$pdf->Cell(25, 10, mb_convert_encoding('Extensión', 'ISO-8859-1', 'UTF-8'), 1, 0, 'C');
-$pdf->Cell(55, 10, mb_convert_encoding('Correo', 'ISO-8859-1', 'UTF-8'), 1, 0, 'C');
+
+// Configurar el color de fondo de la primera fila con letras blancas
+$pdf->SetFillColor(15, 107, 165);
+$pdf->SetTextColor(255, 255, 255);
+
+$pdf->SetX(15);
+$pdf->Cell(58, 10, mb_convert_encoding('Puesto', 'ISO-8859-1', 'UTF-8'), 1, 0, 'C', true);
+$pdf->Cell(30, 10, mb_convert_encoding('Nombre', 'ISO-8859-1', 'UTF-8'), 1, 0, 'C', true);
+$pdf->Cell(40, 10, mb_convert_encoding('Apellidos', 'ISO-8859-1', 'UTF-8'), 1, 0, 'C', true);
+$pdf->Cell(28, 10, mb_convert_encoding('Oficina', 'ISO-8859-1', 'UTF-8'), 1, 0, 'C', true);
+$pdf->Cell(25, 10, mb_convert_encoding('Teléfono', 'ISO-8859-1', 'UTF-8'), 1, 0, 'C', true);
+$pdf->Cell(25, 10, mb_convert_encoding('Extensión', 'ISO-8859-1', 'UTF-8'), 1, 0, 'C', true);
+$pdf->Cell(55, 10, mb_convert_encoding('Correo', 'ISO-8859-1', 'UTF-8'), 1, 0, 'C', true);
 $pdf->Ln(); // Salto de línea
 
 // Mostrar los datos de la tabla en el PDF
 $pdf->SetFont('Arial', '', 10);
 while ($row = $result->fetch_assoc()) {
+    $pdf->SetX(15);
+    $pdf->SetTextColor(0, 0, 0);
     $pdf->Cell(58, 10, mb_convert_encoding($row['puesto'], 'ISO-8859-1', 'UTF-8'), 1, 0, 'C');
     $pdf->Cell(30, 10, mb_convert_encoding($row['nombre'], 'ISO-8859-1', 'UTF-8'), 1, 0, 'C');
     $pdf->Cell(40, 10, mb_convert_encoding($row['apellidos'], 'ISO-8859-1', 'UTF-8'), 1, 0, 'C');
