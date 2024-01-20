544932cd4dc79025a1583b734a6486cd8a249725
dsanguti <dsanguti@gmail.com>
2024-01-20 23:25:11 +0100

    panelcontrol ya puedo editar y eliminar

--- seccion/panelcontrol/modal/modal_eliminar_pc.php
+++ seccion/panelcontrol/modal/modal_eliminar_pc.php
@@ -3,8 +3,8 @@
         <div class="col">
 
             <!-- Modal -->
-            <div class="modal fade" id="modal-del-directorio<?php echo $row['id']; ?>" tabindex="-1"
-                aria-labelledby="modal-del-directorio" aria-hidden="true">
+            <div class="modal fade" id="modal-del-pc<?php echo $row['id']; ?>" tabindex="-1"
+                aria-labelledby="modal-del-pc" aria-hidden="true">
                 <div class="modal-dialog">
                     <div class="modal-content">
                         <div class="modal-header modal-header-delete">
@@ -12,14 +12,15 @@
                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div class="modal-body">
-                            <p> ¿Estas seguro que deseas eliminar a la siguiente persona del directorio?</p>
-                            <h4><?php echo $row['nombre'] . " " . $row['apellidos']; ?></h4>
+                            <p> ¿Estas seguro que deseas eliminar al siguiente usuario?</p>
+                            <h4><?php echo $row['nombre'] . " " . $row['apellido'] . " con usuario " . $row['usuario']; ?>
+                            </h4>
                         </div>
                         <div class="modal-footer">
                             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
                             <a class="btn btn-danger"
-                                href="/myproyecto/seccion/directorio/eliminar_directorio.php?id=<?php echo $row['id']; ?>"
-                                name="eliminar_directorio" role="button"><i class="bi bi-trash"></i> Eliminar</a>
+                                href="/myproyecto/seccion/panelcontrol/eliminar_pc.php?id=<?php echo $row['id']; ?>"
+                                name="eliminar_PC" role="button"><i class="bi bi-trash"></i> Eliminar</a>
                         </div>
                     </div>
                 </div>
