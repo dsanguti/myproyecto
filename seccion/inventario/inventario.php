5135132425ffd19c363a32122a4ce4cdcf0ac3ca
dsanguti <dsanguti@gmail.com>
2024-01-21 10:42:00 +0100

    navbarInventario css

--- seccion/inventario/inventario.php
+++ seccion/inventario/inventario.php
@@ -1,21 +1,19 @@
 <div class="container-fluid vh-100">
     <div class="row h-10">
         <!-- Contenido de la fila adicional al principio -->
-        <div class="col-md-12" style=" padding-top:10px; text-align: center; background-color: #d0d0d0;">
+        <div class="col-md-12" style=" padding-top:10px; text-align: center; background-color: cadetblue;">
             <h2>Inventario</h2>
 
         </div>
     </div>
     <div class="row h-100">
         <!-- Columna 1: Ocupa el 15% del ancho total -->
-        <div class="col-md-2 bg-light d-flex flex-column justify-content-center align-items-center colum1Inventario"
-            style="background-color: #f0f0f0;">
+        <div class="col-md-2 bg-light d-flex flex-column justify-content-center align-items-center colum1Inventario">
             <!-- Contenido de la primera columna -->
             <!-- Navbar Vertical -->
-            <nav class="navbar navbar-expand navbar-light">
+            <nav class="navbar navbar-expand navbar-light navbarInventario" style="background-color:#3498db;transition: background-color 0.3s ease;">
                 <!-- Contenido del Navbar -->
-                <ul
-                    class="navbar-nav flex-column justify-content-center align-items-center mynavbarInventario mynavbarEtiquetas">
+                <ul class="navbar-nav flex-column justify-content-center align-items-center myUlInventario myUlEtiquetas">
                     <li class="nav-item liInventario">
                         <a class="nav-link" href="#/inventario/UCI">UCI</a>
                     </li>
