9181fb553909a72a4ec062327d1b921bc6b323d5
dsanguti <dsanguti@gmail.com>
2024-01-26 23:42:55 +0100

    Con CSS de inventario liado

--- seccion/inventario/inventario.php
+++ seccion/inventario/inventario.php
@@ -1,17 +1,17 @@
 <div class="container-fluid p-0">
     <div style="margin:0px; padding:0px;" class="row w-100">
         <!-- Contenido de la fila adicional al principio -->
-        <div class="col-md-12" style=" padding-top:10px; text-align: center; background-color: cadetblue;">
+        <div class="col-md-12" style=" padding-top:10px; text-align: center;">
             <h2>Inventario</h2>
 
         </div>
     </div>
-    <div style="margin:0px; padding:0px; height:100vh;" class="row w-100">
+    <div style="margin:0px; padding:0px;height:800px" class="row w-100">
         <!-- Columna 1: Ocupa el 15% del ancho total -->
-        <div class="col-md-2 bg-light h-100 d-flex flex-column justify-content-center align-items-center colum1Inventario">
+        <div class="col-md-2 h-100 d-flex flex-column justify-content-center align-items-center colum1Inventario">
             <!-- Contenido de la primera columna -->
             <!-- Navbar Vertical -->
-            <nav class="navbar navbar-expand navbar-light navbarInventario" style="background-color:#3498db;transition: background-color 0.3s ease;">
+            <nav class="navbar navbar-expand navbar-light navbarInventario">
                 <!-- Contenido del Navbar -->
                 <ul class="navbar-nav flex-column justify-content-center align-items-center myUlInventario myUlEtiquetas">
                     <li class="nav-item liInventario">
