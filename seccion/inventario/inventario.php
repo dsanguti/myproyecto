aef32a2d79b0738b38c57c173e04ecd13eacb3f7
dsanguti <dsanguti@gmail.com>
2024-01-27 15:39:49 +0100

    Navbar Intentario CSS

--- seccion/inventario/inventario.php
+++ seccion/inventario/inventario.php
@@ -6,15 +6,17 @@
 
         </div>
     </div>
-    <div style="margin:0px; padding:0px;height:800px" class="row w-100">
+    <div style="margin:0px; padding:0px;height:780px" class="row w-100">
         <!-- Columna 1: Ocupa el 15% del ancho total -->
         <div class="col-md-2 h-100 d-flex flex-column justify-content-center align-items-center colum1Inventario">
             <!-- Contenido de la primera columna -->
             <!-- Navbar Vertical -->
-            <nav class="navbar navbar-expand navbar-light navbarInventario">
+            <nav style="margin:0px; padding:0px;" class="navbar navbar-expand navbar-light navbarInventario">
                 <!-- Contenido del Navbar -->
-                <ul class="navbar-nav flex-column justify-content-center align-items-center myUlInventario myUlEtiquetas">
-                    <li class="nav-item liInventario">
+                <ul
+                    class="navbar-nav flex-column justify-content-center align-items-center myUlInventario myUlEtiquetas">
+                    <li style="border-top-right-radius: 10px;border-top-left-radius: 10px;"
+                        class="nav-item liInventario">
                         <a class="nav-link" href="#/inventario/UCI">UCI</a>
                     </li>
                     <li class="nav-item liInventario">
@@ -23,7 +25,8 @@
                     <li class="nav-item liInventario">
                         <a class="nav-link" href="#/inventario/OE">OE</a>
                     </li>
-                    <li class="nav-item liInventario">
+                    <li style="border-bottom-right-radius: 10px;border-bottom-left-radius: 10px;"
+                        class="nav-item liInventario">
                         <a class="nav-link" href="#/inventario/COE">COE</a>
                     </li>
                 </ul>
