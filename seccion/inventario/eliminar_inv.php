09886267ed55c551625e583654c7de9733b2a427
dsanguti <dsanguti@gmail.com>
2024-03-06 13:33:31 +0100

    Ya funciona modal del inventarioCOE

--- seccion/inventario/eliminar_inv.php
+++ seccion/inventario/eliminar_inv.php
@@ -4,20 +4,20 @@ ini_set('display_errors', 1);
 
 session_start();
 
-include_once('../../bd/panelcontrol/conector_BD_usuarios.php');
+include_once('../../bd/inventario/conector_BD_inventario.php');
 
 if (isset($_GET['id']) && is_numeric($_GET['id'])) {
-    $database = new ConectarBD_PC();
+    $database = new ConectarBD_INV();
     $db = $database->abrir();
 
     try {
         $id = $_GET['id'];
 
-        $stmt = $db->prepare("DELETE FROM usuarios WHERE id = :id");
+        $stmt = $db->prepare("DELETE FROM inventario WHERE id = :id");
         $stmt->bindParam(':id', $id, PDO::PARAM_INT);
         $stmt->execute();
 
-        $_SESSION['message'] = ($stmt->rowCount() > 0) ? 'Persona eliminada correctamente' : 'No se pudo eliminar al usuario';
+        $_SESSION['message'] = ($stmt->rowCount() > 0) ? 'Elemento eliminado correctamente' : 'No se pudo eliminar el elemento del inventario';
     } catch (PDOException $e) {
         $_SESSION['message'] = 'Error en la base de datos: ' . $e->getMessage();
     }
@@ -27,4 +27,4 @@ if (isset($_GET['id']) && is_numeric($_GET['id'])) {
     $_SESSION['message'] = 'Oops..! Seleccione al usuario a eliminar';
 }
 
-header('location: http://localhost/myproyecto/#/panelcontrol');
+header('location: http://localhost/myproyecto/#/inventario/COE');
--- seccion/inventario/modal/modal_eliminar_inv.php
+++ seccion/inventario/modal/modal_eliminar_inv.php
@@ -3,7 +3,7 @@
         <div class="col">
 
             <!-- Modal -->
-            <div class="modal fade" id="modal-del-inventario<?php echo $row['id']; ?>" tabindex="-1" aria-labelledby="modal-del-pc" aria-hidden="true">
+            <div class="modal fade" id="modal-del-inventario<?php echo $row['id']; ?>" tabindex="-1" aria-labelledby="modal-del-inv" aria-hidden="true">
                 <div class="modal-dialog">
                     <div class="modal-content">
                         <div class="modal-header modal-header-delete">
@@ -11,13 +11,13 @@
                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div class="modal-body">
-                            <p> ¿Estas seguro que deseas eliminar al siguiente usuario?</p>
-                            <h4><?php echo $row['nombre'] . " " . $row['apellido'] . " con usuario " . $row['usuario']; ?>
+                            <p> ¿Estas seguro que deseas eliminar al siguiente elemento?</p>
+                            <h4><?php echo "Elemento con cód. " . $row['cod_elemento'] . " modelo: " . $row['modelo']; ?>
                             </h4>
                         </div>
                         <div class="modal-footer">
                             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-                            <a class="btn btn-danger" href="/myproyecto/seccion/panelcontrol/eliminar_pc.php?id=<?php echo $row['id']; ?>" name="eliminar_PC" role="button"><i class="bi bi-trash"></i> Eliminar</a>
+                            <a class="btn btn-danger" href="/myproyecto/seccion/inventario/eliminar_inv.php?id=<?php echo $row['id']; ?>" name="eliminar_INV" role="button"><i class="bi bi-trash"></i> Eliminar</a>
                         </div>
                     </div>
                 </div>
