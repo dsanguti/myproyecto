0a5cf399e95181a71d3a37520065e0015f17dd69
dsanguti <dsanguti@gmail.com>
2024-03-07 18:52:50 +0100

    modifica navBar

--- menuhorizontal.php
+++ menuhorizontal.php
@@ -2,8 +2,7 @@
 <div class="container-fluid">
     <div class="row px-0">
         <div class="col px-0">
-            <nav style="box-shadow: 0px 10px 5px -1px rgba(96,111,212,0.38);"
-                class="navbar navbar navbar-dark bg-primary navbar-expand-lg ">
+            <nav style="box-shadow: 0px 10px 5px -1px rgba(96,111,212,0.38);" class="navbar navbar navbar-dark bg-primary navbar-expand-lg ">
                 <div class="container-fluid">
                     <a class="navbar-brand" href="#/">DSG</a>
                     <?php
@@ -12,59 +11,58 @@
                         echo "<a class='navbar-brand' style='color:#E29930;' href='#/panelcontrol'>Panel Control</a>";
                     }
                     ?>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                        data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false"
-                        aria-label="Toggle navigation">
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse flex-row-reverse" id="navbarNavDropdown">
                         <ul class="navbar-nav">
                             <li class="nav-item dropdown mx-2">
-                                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                                    aria-expanded="false">
-                                    Oficina
+                                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Formación
                                 </a>
                                 <ul class="dropdown-menu">
-                                    <li><a class="dropdown-item" href="#">Empleo</a></li>
-                                    <li><a class="dropdown-item" href="#">Prestaciones</a>
+                                    <li><a class="dropdown-item" href="#">Becas</a></li>
+                            </li>
+                        </ul>
+                        </li>
+                        <li class="nav-item dropdown mx-2">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Prestaciones
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li>
+                                    <a class="dropdown-item submenu-prestaciones" href="#/prestaciones/sanciones">Sanciones</a>
 
-                                    </li>
-                                </ul>
-                            </li>
-                            <li class="nav-item dropdown mx-2">
-                                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                                    aria-expanded="false">
-                                    Dir.Provincial
-                                </a>
-                                <ul class="dropdown-menu">
-                                    <li>
-                                        <a class="dropdown-item submenu-prestaciones"
-                                            href="#/prestaciones">Prestaciones</a>
+                                </li>
+                                <li>
+                                    <a class="dropdown-item submenu-prestaciones" href="#/prestaciones/recl.previas">Recl.Previas</a>
 
-                                    </li>
-                                    <li><a class="dropdown-item" href="#">Formación</a></li>
-                                </ul>
-                            </li>
-                            <li class="nav-item mx-2">
-                                <a class="nav-link Link" id="directorioSeccion" href="#/directorio">Directorio</a>
-                            </li>
-                            <li class="nav-item mx-2">
-                                <a class="nav-link Link" id="inventarioSeccion" href="#/inventario">Inventario</a>
-                            </li>
-                            <li class="nav-item dropdown mx-2">
-                                <a style="color:#E29930;" class="nav-link dropdown-toggle" href="#" role="button"
-                                    data-bs-toggle="dropdown" aria-expanded="false">
-                                    <?php
-                                    echo $_SESSION["nombre"] . " " . $_SESSION["apellido"];
-                                    ?>
-                                </a>
-                                <ul class="dropdown-menu">
-                                    <li>
-                                        <a class="dropdown-item"
-                                            href="./controlador/controlador-cerrar-sesion.php">Cerrar Sesión</a>
-                                    </li>
-                                </ul>
-                            </li>
+                                </li>
+                                <li>
+                                    <a class="dropdown-item submenu-prestaciones" href="#/prestaciones/demandas">Demandas</a>
+
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="nav-item mx-2">
+                            <a class="nav-link Link" id="directorioSeccion" href="#/directorio">Directorio</a>
+                        </li>
+                        <li class="nav-item mx-2">
+                            <a class="nav-link Link" id="inventarioSeccion" href="#/inventario">Inventario</a>
+                        </li>
+                        <li class="nav-item dropdown mx-2">
+                            <a style="color:#E29930;" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <?php
+                                echo $_SESSION["nombre"] . " " . $_SESSION["apellido"];
+                                ?>
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li>
+                                    <a class="dropdown-item" href="./controlador/controlador-cerrar-sesion.php">Cerrar
+                                        Sesión</a>
+                                </li>
+                            </ul>
+                        </li>
                         </ul>
                     </div>
                 </div>
